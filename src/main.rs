f43c3f23599d33073a44ba717588c15561d9a2b4
Tobias Wölfel <tobias.woelfel@mailbox.org>
2024-11-15 07:15:49 +0100

    Separate argument parsing
    Move CLI arguments into a separate file.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod cli;
+
 use clap::Parser;
-use std::{
-    sync::{Arc, Mutex},
-    time::Duration,
-};
+use cli::{parse_duration, Cli};
+
+use std::sync::{Arc, Mutex};
 use tokio::sync::oneshot;
 use tokio::time::sleep;
 use zbus::{proxy, Connection, Result};
@@ -16,45 +17,9 @@ trait SwayNC {
     async fn set_dnd(&self, state: &bool) -> Result<()>;
 }
 
-#[derive(Parser)]
-#[command(name = "wait")]
-#[command(about = "Waits for specified duration")]
-struct Args {
-    /// Duration to wait (e.g. "5s", "2m", "1h")
-    #[arg(default_value = "25m")]
-    duration: String,
-}
-
-fn parse_duration(input: &str) -> Option<Duration> {
-    if input.is_empty() {
-        return None;
-    }
-
-    let (number_part, unit_part) = input
-        .trim()
-        .chars()
-        .partition::<String, _>(|c| c.is_ascii_digit());
-
-    let value: u64 = match number_part.parse() {
-        Ok(num) => num,
-        Err(_) => return None,
-    };
-
-    match unit_part.as_str() {
-        "s" => Some(Duration::from_secs(value)),
-        "m" => Some(Duration::from_secs(value * 60)),
-        "h" => Some(Duration::from_secs(value * 60 * 60)),
-        "d" => Some(Duration::from_secs(value * 60 * 60 * 24)),
-        _ => {
-            println!("{unit_part}");
-            None
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
-    let args = Args::parse();
+    let args = Cli::parse();
 
     let connection = Connection::session().await?;
     let proxy = SwayNCProxy::new(&connection).await?;
