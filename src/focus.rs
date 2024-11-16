63bc501528bb9576d9835fc38f12cebae165f475
Tobias Wölfel <tobias.woelfel@mailbox.org>
2024-11-16 17:05:10 +0100

    Move the logic into a separate file

---
+++ src/focus.rs
@@ -0,0 +1,64 @@
+use std::time::Duration;
+use crate::swaync_interface::SwayNCInterface;
+use crate::notification_interface::NotificationInterface;
+use crate::sway_ipc_interface::SwayIpcInterface;
+
+use anyhow::Result;
+
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+use tokio::sync::oneshot;
+use tokio::time::sleep;
+use zbus::zvariant::Value;
+
+pub async fn run(duration: Duration) -> Result<()> {
+    // Initialize the interfaces
+    let swaync = SwayNCInterface::new().await?;
+    let notify = NotificationInterface::new().await?;
+    let mut sway = SwayIpcInterface::new().await?;
+
+    // Open channels to communicate with the Ctrl+C handler
+    let (tx, rx) = oneshot::channel();
+    let tx = Arc::new(Mutex::new(Some(tx)));
+
+
+    // Set the tools to the desired state
+    swaync.enable_dnd().await?;
+    sway.set_bar_mode_invisible().await?;
+
+
+    // Set the Ctrl+C handler
+    // This is needed so the program does not just end on Ctrl+c, but clean up
+    // before. For example, unset the do not disturb state.
+    let tx_clone = Arc::clone(&tx);
+    ctrlc::set_handler(move || {
+        let mut tx_lock = tx_clone.lock().unwrap();
+        if let Some(tx) = tx_lock.take() {
+            let _ = tx.send(());
+        }
+    })
+    .expect("Error setting Ctrl+C handler");
+
+    // Wait for the `duration` specified time or a Ctrl+C signal
+    tokio::select! {
+        _ = sleep(duration) => {},
+        _ = rx => {
+            println!("\nReceived Ctrl+C, starting cleanup...");
+        },
+    }
+
+    // Restore the tools and notify the user
+    swaync.disable_dnd().await?;
+    sway.set_bar_mode_dock().await?;
+
+    let mut hints = HashMap::new();
+    hints.insert("urgency", &Value::U8(2));
+
+    let _ = notify.notify(
+        "Focus time over",
+        &format!("{:?} have passed", duration),
+        hints,
+    ).await?;
+
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,18 @@ mod swaync_interface;
 mod notification;
 mod notification_interface;
 mod sway_ipc_interface;
+mod focus;
 
 
 use clap::Parser;
 use cli::{parse_duration, Cli};
 
-use std::{collections::HashMap, sync::{Arc, Mutex}};
-use tokio::sync::oneshot;
-use tokio::time::sleep;
-use zbus::{zvariant::Value};
-
 use anyhow::Result;
 
-use swaync_interface::SwayNCInterface;
-use notification_interface::NotificationInterface;
-use sway_ipc_interface::SwayIpcInterface;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
-
-    let swaync = SwayNCInterface::new().await?;
-    let notify = NotificationInterface::new().await?;
-    let mut sway = SwayIpcInterface::new().await?;
-
-    swaync.enable_dnd().await?;
 
     let duration = if let Some(duration) = parse_duration(&args.duration) {
         println!("Setting DND for {:?}", duration);
@@ -38,39 +25,7 @@ async fn main() -> Result<()> {
         std::process::exit(1);
     };
 
-    sway.set_bar_mode_invisible().await?;
-
-    let (tx, rx) = oneshot::channel();
-    let tx = Arc::new(Mutex::new(Some(tx)));
-
-    let tx_clone = Arc::clone(&tx);
-    ctrlc::set_handler(move || {
-        let mut tx_lock = tx_clone.lock().unwrap();
-        if let Some(tx) = tx_lock.take() {
-            let _ = tx.send(());
-        }
-    })
-    .expect("Error setting Ctrl+C handler");
-
-    tokio::select! {
-        _ = sleep(duration) => {},
-        _ = rx => {
-            println!("\nReceived Ctrl+C, starting cleanup...");
-        },
-    }
-
-    // Restore the tools and notify the user
-    swaync.disable_dnd().await?;
-    sway.set_bar_mode_dock().await?;
-    
-    let mut hints = HashMap::new();
-    hints.insert("urgency", &Value::U8(2));
-
-    let _ = notify.notify(
-        "Focus time over",
-        &format!("{:?} have passed", duration),
-        hints,
-    ).await?;
+    focus::run(duration).await?;
 
     Ok(())
 }
